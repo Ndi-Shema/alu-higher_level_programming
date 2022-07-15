73789e39248153b53abe7b0de4c9c416b6941c80
root <f.shema@alustudent.t.com>
2022-07-15 12:32:43 -0700

    more returns

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = len(sentence)
-    b = []
-    y = []
-    if a < 1:
-        b.append("None")
-        b.append(a)
-
+    x = []
+    len_s = len(sentence)
+    if len_s < 1:
+        x.append(len_s)
+        x.append("None")
     else:
-        b.append(sentence[0])
-        b.append(a)
-        y = tuple(b)
-        return y
+        x.append(len_s)
+        x.append(sentence[0])
+    y = tuple(x)
+    return y
