8a3f518fac5a36d442bacd81b93716c719ce8f42
root <f.shema@alustudent.t.com>
2022-07-15 05:49:13 -0700

    more returns

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+def multiple_returns(sentence):
+    a = len(sentence)
+    if a == 0:
+        b = list(sentence)
+        b.append("None")
+        return b
+    else:
+        c = list(sentence)
+        return c[0]
