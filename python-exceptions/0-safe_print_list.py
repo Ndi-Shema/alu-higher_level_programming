d6cff79e0b64cdba8ee8e4805be4b5a81c76c952
root <f.shema@alustudent.t.com>
2022-07-22 03:00:38 -0700

    Safe print list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,18 +2,13 @@
 
 
 def safe_print_list(my_list=[], x=0):
-       count = 0
-       try:
-           while count < x:
-               print(my_list[count], end="")
-               count += 1
-
-        except IndexError:
-
-            pass
-
-        finally:
-
-            print()
-
-            return count
+    count = 0
+    try:
+        while count < x:
+            print(my_list[count], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
